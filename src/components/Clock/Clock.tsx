c59a9ac84440d5c58732a7ba7c37885a3f026c7c
czearing <czearing@outlook.com>
2024-03-10 14:14:52 -0700

    Adding date and updating styles

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { Card, tokens, makeStyles } from "@fluentui/react-components";
+import {
+  Card,
+  LargeTitle,
+  Subtitle1,
+  tokens,
+  makeStyles,
+  shorthands,
+} from "@fluentui/react-components";
 
 const useStyles = makeStyles({
   clockContainer: {
@@ -8,34 +15,50 @@ const useStyles = makeStyles({
     alignItems: "center",
     height: "200px",
     width: "250px",
+    ...shorthands.gap(tokens.spacingHorizontalM),
   },
   clock: {
     fontSize: tokens.fontSizeHero900,
+  },
+  date: {
+    fontSize: tokens.fontSizeBase600,
   },
 });
 
 export const Clock = () => {
   const [currentTime, setCurrentTime] = React.useState("");
+  const [currentDate, setCurrentDate] = React.useState("");
   const styles = useStyles();
 
   React.useEffect(() => {
-    const interval = setInterval(() => {
+    const updateDateTime = () => {
       const now = new Date();
       let hours = now.getHours();
       const minutes = now.getMinutes().toString().padStart(2, "0");
-      const seconds = now.getSeconds().toString().padStart(2, "0");
+
       const ampm = hours >= 12 ? "PM" : "AM";
       hours = hours % 12;
-      hours = hours ? hours : 12;
-      setCurrentTime(`${hours}:${minutes}:${seconds} ${ampm}`);
-    }, 1000);
+      hours = hours ? hours : 12; // Convert hour '0' to '12'
+      setCurrentTime(`${hours}:${minutes} ${ampm}`);
+
+      const options: Intl.DateTimeFormatOptions = {
+        weekday: "short",
+        day: "numeric",
+        month: "short",
+      };
+      setCurrentDate(now.toLocaleDateString(undefined, options));
+    };
+
+    const interval = setInterval(updateDateTime, 1000);
+    updateDateTime(); // Initial update
 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <Card className={styles.clockContainer} appearance="filled">
-      <div className={styles.clock}>{currentTime}</div>
+      <Subtitle1 className={styles.date}>{currentDate}</Subtitle1>
+      <LargeTitle className={styles.clock}>{currentTime}</LargeTitle>
     </Card>
   );
 };
