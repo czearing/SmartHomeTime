a9ba6146cfe1dcdf2cfca0a308802894125ba1f5
czearing <czearing@outlook.com>
2024-03-10 20:10:40 -0700

    Making the theme switch exactly when sun sets and rises

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,6 +1,20 @@
 import * as React from "react";
-import { makeStyles, shorthands } from "@fluentui/react-components";
-const appContainerStyles = { height: "100%" };
+import {
+  makeStyles,
+  shorthands,
+  FluentProvider,
+  tokens,
+  webLightTheme,
+  webDarkTheme,
+} from "@fluentui/react-components";
+import { useQuery } from "@tanstack/react-query";
+import { fetchWeather } from "../../server";
+import { WeatherProvider } from "src/context";
+
+const fluentProviderStyles = {
+  height: "100%",
+  backgroundColor: tokens.colorNeutralBackground2,
+};
 
 const useStyles = makeStyles({
   container: {
@@ -9,10 +23,103 @@ const useStyles = makeStyles({
   },
 });
 
+const mockData = {
+  dailyData: {
+    timestep: "1d",
+    endTime: "2024-03-11T06:00:00-07:00",
+    startTime: "2024-03-10T06:00:00-07:00",
+    intervals: [
+      {
+        startTime: "2024-03-10T06:00:00-07:00",
+        values: {
+          humidity: 92,
+          precipitationProbability: 98,
+          sunriseTime: "2024-03-10T14:29:00Z",
+          sunsetTime: "2024-03-11T02:06:00Z",
+          temperature: 45.84,
+          weatherCode: 4200,
+          windSpeed: 9.77,
+        },
+      },
+      {
+        startTime: "2024-03-11T06:00:00-07:00",
+        values: {
+          humidity: 85.97,
+          precipitationProbability: 95,
+          sunriseTime: "2024-03-11T14:27:00Z",
+          sunsetTime: "2024-03-12T02:07:00Z",
+          temperature: 45.8,
+          weatherCode: 4200,
+          windSpeed: 12.92,
+        },
+      },
+    ],
+  },
+  firstDay: {
+    humidity: 92,
+    precipitationProbability: 98,
+    sunriseTime: "2024-03-10T14:29:00Z",
+    sunsetTime: "2024-03-11T02:06:00Z",
+    temperature: 45.84,
+    weatherCode: 4200,
+    windSpeed: 9.77,
+  },
+};
+
 export const AppContainer: React.FC<{ children: React.ReactNode }> = (
   props
 ) => {
+  const [theme, setTheme] = React.useState(webLightTheme);
   const styles = useStyles();
 
-  return <div className={styles.container}>{props.children}</div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["weather", location!],
+    // TODO: Replace any with the correct type, I'm lazy atm
+    queryFn: (context) => fetchWeather(context as any),
+    enabled: !!location,
+    staleTime: 1000 * 60 * 60, // 1 hour
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+
+  React.useEffect(() => {
+    const updateThemeBasedOnTime = () => {
+      const now = new Date();
+      const sunriseTime = mockData && new Date(mockData.firstDay.sunriseTime);
+      const sunsetTime = mockData && new Date(mockData.firstDay.sunsetTime);
+
+      if (sunriseTime && sunsetTime && now >= sunriseTime && now < sunsetTime) {
+        // If the current time is after sunrise but before sunset, use the light theme
+        setTheme(webLightTheme);
+      } else {
+        // If the current time is after sunset or before sunrise, use the dark theme
+        setTheme(webDarkTheme);
+      }
+    };
+
+    // Calculate the milliseconds until the next hour to align theme updates
+    const now = new Date();
+    const msUntilNextHour =
+      (60 - now.getMinutes()) * 60 * 1000 - now.getSeconds() * 1000;
+    const timeoutId = setTimeout(() => {
+      updateThemeBasedOnTime();
+
+      // After the first timeout, set an interval to update the theme every hour
+      setInterval(updateThemeBasedOnTime, 3600000);
+    }, msUntilNextHour);
+
+    updateThemeBasedOnTime();
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <FluentProvider theme={theme} style={fluentProviderStyles}>
+      <WeatherProvider value={{ weather: mockData! }}>
+        <div className={styles.container}>{props.children}</div>
+      </WeatherProvider>
+    </FluentProvider>
+  );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import * as React from "react";
-import {
-  FluentProvider,
-  webLightTheme,
-  webDarkTheme,
-  tokens,
-} from "@fluentui/react-components";
+import { tokens } from "@fluentui/react-components";
 import { SSRProvider } from "@fluentui/react-utilities";
-import { useLocalDefault, useThemeDetector, useGetLocal } from "../utils";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { RendererProvider, createDOMRenderer } from "@griffel/react";
@@ -14,52 +8,13 @@ import { AppContainer } from "../components";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../clients";
 
-const fluentProviderStyles = {
-  height: "100%",
-  backgroundColor: tokens.colorNeutralBackground2,
-};
-
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
   const [isMounted, setIsMounted] = React.useState(false);
 
-  // 1. Get the default theme from local storage.
-  useLocalDefault("theme", "system");
-
-  const [theme, setTheme] = React.useState(webLightTheme);
-
   React.useEffect(() => {
     setIsMounted(true);
-
-    const updateThemeBasedOnTime = () => {
-      const now = new Date();
-      const hour = now.getHours();
-
-      // Switch to dark theme after 7 PM and before 7 AM
-      if (hour >= 19 || hour < 7) {
-        setTheme(webLightTheme);
-      } else {
-        setTheme(webLightTheme);
-      }
-    };
-
-    // Calculate the milliseconds until the next hour to align theme updates
-    const now = new Date();
-    const msUntilNextHour =
-      (60 - now.getMinutes()) * 60 * 1000 - now.getSeconds() * 1000;
-    const timeoutId = setTimeout(() => {
-      updateThemeBasedOnTime();
-
-      // After the first timeout, set an interval to update the theme every hour
-      setInterval(updateThemeBasedOnTime, 3600000);
-    }, msUntilNextHour);
-
-    updateThemeBasedOnTime();
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
   }, []);
 
   return (
@@ -75,7 +30,7 @@ export default function App(props: AppProps) {
       </Head>
       <style jsx global>{`
         body {
-          background-color: ${theme.colorNeutralBackground1};
+          background-color: ${tokens.colorNeutralBackground1};
           padding: 0px;
           margin: 0px;
           height: 100%;
@@ -91,11 +46,9 @@ export default function App(props: AppProps) {
       <RendererProvider renderer={pageProps.renderer || createDOMRenderer()}>
         <SSRProvider>
           {isMounted && (
-            <FluentProvider theme={theme} style={fluentProviderStyles}>
-              <AppContainer>
-                <Component {...pageProps} />
-              </AppContainer>
-            </FluentProvider>
+            <AppContainer>
+              <Component {...pageProps} />
+            </AppContainer>
           )}
         </SSRProvider>
       </RendererProvider>
