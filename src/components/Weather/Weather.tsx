95607b6e8af65ecfd1b215c47979d2df5d337ccf
czearing <czearing@outlook.com>
2024-03-10 19:05:01 -0700

    Improving ui

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import {
   Card,
   LargeTitle,
-  Subtitle1,
   Subtitle2,
   tokens,
   makeStyles,
@@ -10,11 +9,10 @@ import {
   Body1,
   Spinner,
   Body1Strong,
-  Divider,
 } from "@fluentui/react-components";
 import { WeatherIcon } from "./WeatherIcon";
 import { WeatherContext } from "../../context";
-import { formatTime, weatherCode } from "../../utils";
+import { weatherCode } from "../../utils";
 
 const useStyles = makeStyles({
   weatherContainer: {
@@ -22,12 +20,14 @@ const useStyles = makeStyles({
     justifyContent: "center",
     alignItems: "center",
     height: "200px",
-    width: "380px",
+    width: "330px",
     ...shorthands.gap(tokens.spacingHorizontalM),
+    // TODO: Add specific gradients based on the weather
     // backgroundImage: "linear-gradient(to bottom, #d3d3d3, #a9a9a9)",
   },
   columnContainer: {
     display: "flex",
+    marginBottom: "-35px",
     flexWrap: "nowrap",
     flexDirection: "column",
     ...shorthands.gap(tokens.spacingVerticalM),
@@ -38,6 +38,25 @@ const useStyles = makeStyles({
     flexDirection: "row",
     ...shorthands.gap(tokens.spacingHorizontalS),
   },
+  mainContainer: {
+    display: "flex",
+    ...shorthands.gap(tokens.spacingHorizontalXXXL),
+    alignItems: "center",
+    height: "100%",
+  },
+  weatherTextContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    ...shorthands.gap(tokens.spacingVerticalM),
+  },
+  weatherIconContainer: {
+    display: "flex",
+    ...shorthands.gap(tokens.spacingHorizontalM),
+  },
+  flex: {
+    display: "flex",
+  },
 });
 
 export const Weather = () => {
@@ -47,32 +66,21 @@ export const Weather = () => {
   return (
     <Card className={styles.weatherContainer} appearance="filled">
       {weather ? (
-        <div
-          style={{
-            display: "flex",
-            gap: tokens.spacingHorizontalXXXL,
-            alignItems: "center",
-            height: "100%",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              gap: tokens.spacingVerticalM,
-            }}
-          >
-            <div className={styles.columnContainer}>
+        <div className={styles.mainContainer}>
+          <div className={styles.weatherTextContainer}>
+            <div className={styles.weatherIconContainer}>
               <WeatherIcon weatherCode={weather?.firstDay?.weatherCode} />
-              <Subtitle2>
-                {weatherCode[weather?.firstDay?.weatherCode]}
-              </Subtitle2>
+              <div className={styles.flex}>
+                <LargeTitle>
+                  {Math.round(weather?.firstDay?.temperature)}
+                </LargeTitle>
+                <Body1Strong>°F</Body1Strong>
+              </div>
             </div>
-            <LargeTitle>
-              {Math.round(weather?.firstDay?.temperature) + " F"}
-            </LargeTitle>
+
+            <Subtitle2>{weatherCode[weather?.firstDay?.weatherCode]}</Subtitle2>
           </div>
-          {/* <Divider vertical style={{ height: "100%" }} /> */}
+
           <div className={styles.columnContainer}>
             <div className={styles.rowContainer}>
               <Body1Strong>Wind</Body1Strong>
@@ -82,27 +90,7 @@ export const Weather = () => {
               <Body1Strong>Humidity</Body1Strong>
               <Body1>{Math.round(weather?.firstDay?.humidity)}%</Body1>
             </div>
-            {/* <div className={styles.rowContainer}>
-              <Body1Strong>Sunrise</Body1Strong>
-              <Body1>{formatTime(weather?.firstDay?.sunriseTime)}</Body1>
-            </div>
-            <div className={styles.rowContainer}>
-              <Body1Strong>Sunset</Body1Strong>
-              <Body1>{formatTime(weather?.firstDay?.sunsetTime)}</Body1>
-            </div> */}
           </div>
-
-          {/* <div className={styles.columnContainer}>
-            <div className={styles.rowContainer}>
-              <Body1Strong>Sunrise</Body1Strong>
-              <Body1>{formatTime(weather?.firstDay?.sunriseTime)}</Body1>
-            </div>
-
-            <div className={styles.rowContainer}>
-              <Body1Strong>Sunset</Body1Strong>
-              <Body1>{formatTime(weather?.firstDay?.sunsetTime)}</Body1>
-            </div>
-          </div> */}
         </div>
       ) : (
         <Spinner />
