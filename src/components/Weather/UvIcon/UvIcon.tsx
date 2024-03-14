818004449a4e04991aa5e4f7fd0b6da0ddff8335
czearing <czearing@outlook.com>
2024-03-14 16:17:36 -0700

    Updating UvIcon

--- src/components/Weather/UvIcon/UvIcon.tsx
+++ src/components/Weather/UvIcon/UvIcon.tsx
@@ -5,9 +5,14 @@ import Image from "next/image";
 export const UvIcon: React.FC<UvIconProps> = (props) => {
   const { uvIndex } = props;
 
+  console.log(uvIndex);
   return (
     <Image
-      src={`/icons/uv/uv-index-${uvIndex}.svg`}
+      src={
+        uvIndex === 0
+          ? `/icons/uv/uv-index-1.svg`
+          : `/icons/uv/uv-index-${uvIndex}.svg`
+      }
       alt={`UV index: ${uvIndex}`}
       width={32}
       height={32}
